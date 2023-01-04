a6e46039030a74f1c56bf169d0df81d3a662ad48
Felipe Bueno <felipebueno2201@gmail.com>
2023-01-04 12:23:14 -0300

    fix(index to src)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import authRouter from "./src/routes/auth.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 app.use(cors());
