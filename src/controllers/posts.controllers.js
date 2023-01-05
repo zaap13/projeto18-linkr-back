987f84ef23487673ad30d6e69af9cb728d56b23d
Felipe Bueno <felipebueno2201@gmail.com>
2023-01-05 17:12:12 -0300

    insert hashtags

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,12 +1,36 @@
-import { insertPost } from "../repositories/posts.repositories.js";
+import {
+  insertPost,
+  insertTag,
+  checkTag,
+  updateTag,
+  insertHashPost,
+} from "../repositories/posts.repositories.js";
 
 export async function newPost(req, res) {
-    const newPost = req.post;  
+  const newPost = req.post;
+  let hashs;
+  if (newPost.content) {
+    hashs = newPost.content.match(/#\w+/g);
+  }
 
-    try {
-        await insertPost(newPost);
-        res.sendStatus(201);
-    } catch (err) {
-        res.status(500).send(err);
-    };
-};
+  try {
+    const postId = await insertPost(newPost);
+
+    if (hashs) {
+      hashs.forEach(async (hash) => {
+        const hashExists = await checkTag(hash);
+        if (hashExists.rows[0]) {
+          await updateTag(hash);
+          await insertHashPost(postId.rows[0].id, hashExists.rows[0].id);
+        } else {
+          const tagId = await insertTag(hash);
+          await insertHashPost(postId.rows[0].id, tagId.rows[0].id);
+        }
+      });
+    }
+
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}
--- src/repositories/posts.repositories.js
+++ src/repositories/posts.repositories.js
@@ -1,11 +1,48 @@
 import { connection } from "../database/db.js";
 
 export function insertPost(newPost) {
-    const { userId, url, content } = newPost;
+  const { userId, url, content } = newPost;
 
-    return connection.query(`
+  return connection.query(
+    `
         INSERT INTO posts ("userId", url, content)
-        VALUES ($1, $2, $3)`,
-        [userId, url, content]
-    );
-};
+        VALUES ($1, $2, $3)
+        RETURNING id`,
+    [userId, url, content]
+  );
+}
+
+export function insertTag(hash) {
+  return connection.query(
+    `INSERT INTO hashtags (name)
+    VALUES ($1) 
+    RETURNING id`,
+    [hash]
+  );
+}
+
+export function updateTag(hash) {
+  return connection.query(
+    `UPDATE hashtags
+     SET count = count + 1
+     WHERE hashtags.name = '${hash}'`
+  );
+}
+
+export function checkTag(hash) {
+  return connection.query(`
+    SELECT id 
+    FROM hashtags 
+    WHERE hashtags.name = '${hash}'
+`);
+}
+
+export function insertHashPost(postId, tagId) {
+  return connection.query(
+    `
+    INSERT INTO "hashPost" ("postId", "hashtagId")
+    VALUES($1, $2)
+    `,
+    [postId, tagId]
+  );
+}
