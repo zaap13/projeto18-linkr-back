b6bf2dbc24b6858d546dc49d7b4f925d71d2d3fa
Felipe Bueno <felipebueno2201@gmail.com>
2023-01-07 01:35:43 -0300

    Trending

--- src/controllers/hashtags.controller.js
+++ src/controllers/hashtags.controller.js
@@ -1,6 +1,7 @@
 import {
   selectIdByHash,
   selectPostsByHashId,
+  selectTrending,
 } from "../repositories/hashtags.repositories.js";
 
 export async function getPostsByHash(req, res) {
@@ -10,6 +11,17 @@ export async function getPostsByHash(req, res) {
 
     const hashPosts = await selectPostsByHashId(hashId.rows[0].id);
     res.send(hashPosts.rows);
+  } catch (err) {
+    res.status(500).send(err);
+    console.log(err);
+  }
+}
+
+export async function getTrending(req, res) {
+  try {
+    const trending = await selectTrending();
+
+    res.send(trending.rows);
   } catch (err) {
     res.status(500).send(err);
     console.log(err);
--- src/routes/hashtags.routes.js
+++ src/routes/hashtags.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getPostsByHash } from "../controllers/hashtags.controller.js";
+import {
+  getPostsByHash,
+  getTrending,
+} from "../controllers/hashtags.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.get("/hashtag/:hashtag", authMiddleware, getPostsByHash);
+router.get("/trending", authMiddleware, getTrending);
 
 export default router;
