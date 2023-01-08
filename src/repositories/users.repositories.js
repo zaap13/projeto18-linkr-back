e843a9bae4e6f0121f88ad05796aa02adc23aa44
Dayane <acdayane@gmail.com>
2023-01-08 13:42:47 -0300

    fix(send post' likes and who liked)

--- src/repositories/users.repositories.js
+++ src/repositories/users.repositories.js
@@ -10,6 +10,21 @@ export function getUserData(userId) {
 export function getUserPosts(userId) {
 
     return connection.query(`
-        SELECT url, content FROM posts WHERE "userId"=$1;`, [userId]
+        SELECT p.id, p.url, p.content, COUNT(l."userId") AS likes
+        FROM posts p
+        LEFT JOIN likes l ON p.id = l."postId"
+        WHERE p."userId"=$1
+        GROUP BY p.id;`,
+        [userId]
+    );
+};
+
+export function getWhoLiked() {
+
+    return connection.query(`
+        SELECT p.id, p.url, p.content, l."userId", u.username AS "whoLiked"
+        FROM posts p
+        LEFT JOIN likes l ON p.id = l."postId"
+        JOIN users u ON l."userId"=u.id;`
     );
 };
