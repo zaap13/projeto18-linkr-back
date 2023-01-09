424398763bb967f5fa52b59366952b427d97e8c8
Josana-Kla <josana.0205@gmail.com>
2023-01-08 21:44:01 -0300

    feat: added update post route and controller

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -54,12 +54,47 @@ export async function removePost(req, res) {
       const { rows: post } = await findPost(id); 
 
       if(post[0] !== undefined) {
-          await deletePost(id);
-          return res.sendStatus(200);
+        await deletePost(id);
+        return res.sendStatus(200);
       } else {
-          return res.sendStatus(404); 
+        return res.sendStatus(404); 
       }
     } catch (err) {
-        return res.status(500).send(err);
+      return res.status(500).send(err);
     }
 }
+
+export async function updatePost(req, res) {
+  const { id: postId } = req.params;
+  const contentPost = req.post;
+  const validation = postsSchema.validate(req.body, {abortEarly: false});
+  let hashs;
+
+  if (contentPost.content) {
+    hashs = contentPost.content.match(/#\w+/g);
+  };
+
+  if(validation.error) {
+    const error = validation.error.details.map(detail => detail.message);
+
+    return res.status(400).send(error);
+  };
+
+  try {
+    const { rows: postIdExists } = await connection.query(`
+      SELECT * FROM posts WHERE id = $1;
+    `, [postId]);
+
+    if(postIdExists[0]) {
+      await connection.query(`
+        UPDATE posts SET content = $1 WHERE id = $2;
+      `, [contentPost, postId]);
+
+      return res.sendStatus(204);
+    } else {
+      return res.sendStatus(404);
+    };
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { postsMiddleware } from "../middlewares/posts.middleware.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { getAllPosts, newPost, removePost } from "../controllers/posts.controllers.js";
+import { getAllPosts, newPost, removePost, updatePost } from "../controllers/posts.controllers.js";
 
 const router = Router();
 
@@ -10,5 +10,7 @@ router.post("/posts", authMiddleware, postsMiddleware, newPost);
 router.get("/posts", authMiddleware, getAllPosts);
 
 router.delete("/posts/:id", authMiddleware, removePost);
+
+router.patch("/posts/:id", authMiddleware, updatePost);
 
 export default router;
