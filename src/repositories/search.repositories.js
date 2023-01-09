89219150967e524e10c4b0943cc6a0f44890b65d
Dayane <acdayane@gmail.com>
2023-01-09 15:20:39 -0300

    fix (ilike on search)

--- src/repositories/search.repositories.js
+++ src/repositories/search.repositories.js
@@ -4,6 +4,6 @@ export function getUsers(input) {
 
     return connection.query(`
         SELECT id, username, picture FROM users
-        WHERE username LIKE '%${input}%' ORDER BY username LIMIT 10;`,
+        WHERE username ILIKE '%${input}%' ORDER BY username LIMIT 10;`,
     );
 };
