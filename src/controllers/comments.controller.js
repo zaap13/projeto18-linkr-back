a59075f9680c5d37709153c17166c3b9bbad4595
Josana-Kla <josana.0205@gmail.com>
2023-01-13 13:42:28 -0300

    feat: created comments repositories and removed get comments route

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,28 +1,15 @@
 import { connection } from "../database/db.js";
+import { insertComment } from "../repositories/comments.repositories.js";
 
 export async function newComment(req, res) {
     const postId = req.params.postId;
-    const { content, userId } = req.post;
+    const newComment = req.post;
 
     try {
-        await connection.query(`
-            INSERT INTO comments(content, "userId", "postId") VALUES($1, $2, $3)
-        `, [content, userId, postId]);
+        await insertComment(newComment, postId);
 
         return res.sendStatus(201);
     } catch (err) {
         return res.status(500).send(err);
-    }
-}
-
-export async function getComments(req, res) {
-    try {
-        const { rows: allComments } = await connection.query(`
-            SELECT * FROM comments;
-        `);
-
-        return res.send(allComments);
-    } catch (error) {
-        res.send(error);
     }
 }
--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { getComments, newComment } from "../controllers/comments.controller.js";
+import { newComment } from "../controllers/comments.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { commentsMiddleware } from "../middlewares/comments.middlewares.js";
 
 const router = Router();
 
 router.post("/comments/:postId", authMiddleware, commentsMiddleware, newComment);
-router.get("/comments", authMiddleware, getComments);
 
 export default router;
